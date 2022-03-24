8172929e7a0427c7f7ebad66721b3ba7ea1a34c0
terry <terryjnh@gmail.com>
2022-03-24 22:45:43 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { RootStore } from "./Store";
+import { GetPokemon } from "./actions/PokemonActions";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 
 function App() {
+  const dispatch = useDispatch();
+  const [pokemonName, setPokemonName] = useState("");
+  const pokemonState = useSelector((state: RootStore) => state.pokemon);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPokemonName(event.target.value);
+  };
+  const handleSubmit = () => {
+    dispatch(GetPokemon(pokemonName));
+  };
+
+  console.log("pokemon state:", pokemonState);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{ margin: "auto", width: "100%", maxWidth: 800 }}>
+        <Typography sx={{ mt: 6 }} variant="h3" component="div" gutterBottom>
+          Pokemon Encyclopedia
+        </Typography>
+
+        <Grid container spacing={1}>
+          <Grid item xs={9}>
+            <TextField fullWidth
+              id="pokemon-search-field"
+              label="Type here (eg. pikachu)"
+              variant="filled"
+              onChange={handleChange}
+            ></TextField>
+          </Grid>
+          <Grid item xs={3}>
+            <Button
+              sx={{ margin: "auto", mt: 1.5 }}
+              variant="contained"
+              onClick={handleSubmit}
+            >
+              Search
+            </Button>
+          </Grid>
+        </Grid>
+
+        {pokemonState.pokemon && (
+          <div>
+            <img src={pokemonState.pokemon.sprites.front_default} alt="" />
+            {pokemonState.pokemon?.abilities.map((ability) => {
+              return <p>{ability.ability.name}</p>;
+            })}
+          </div>
+        )}
+      </Box>
     </div>
   );
 }
